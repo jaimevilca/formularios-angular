368ee78a90d4bfdfd949916c63b65e0417c1c6b1
Jaime Vilca <jaime_v@outlook.com>
2017-05-18 22:14:06 -0500

    Formularios (data)

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'app-data',
@@ -7,9 +9,120 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+
+  usuario: Object = {
+    nombreCompleto: {
+      nombre: 'jaime',
+      apellido: 'andres'
+    },
+    correo: 'jaime@test.com',
+    pasatiempos: []
+  }
+
+  constructor() {
+    this.forma = new FormGroup({
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl('', [Validators.required, Validators.minLength(3), this.noJaime]),
+        'apellido': new FormControl('', Validators.required),
+      }),
+
+      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      'pasatiempos': new FormArray([
+        new FormControl('correr', Validators.required)
+      ]),
+      'username': new FormControl('', Validators.required, this.existeUsuario),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
+    });
+
+
+    this.forma.controls['password2'].setValidators([
+      Validators.required, this.noIgual.bind(this.forma)
+    ]);
+
+
+    // this.forma.valueChanges.subscribe( data => {
+    this.forma.controls["username"].valueChanges
+      .subscribe(data => {
+        console.log(data);
+      });
+
+
+    this.forma.controls["username"].statusChanges
+      .subscribe(data => {
+        console.log(data);
+      });
+
+
+
+    // this.forma.setValue(this.usuario);
+  }
 
   ngOnInit() {
+  }
+
+  guardarCambios() {
+    console.log(this.forma);
+    // this.forma.reset({
+    //     nombreCompleto: {
+    //         nombre: '',
+    //         apellido: ''
+    //       },
+    //       correo: ''
+    // });
+  }
+
+
+  agregarPasatiempo() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(new FormControl('', Validators.required));
+  }
+
+
+  noJaime(control: FormControl): { [s: string]: boolean } {
+
+    if (control.value == 'jaime') {
+      return {
+        nojaime: true
+      }
+    }
+
+    return null;
+
+  }
+
+
+  existeUsuario(control: FormControl): Promise<any> | Observable<any> {
+
+    let promesa = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value == 'strider') {
+          resolve({ existe: true })
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
+
+    return promesa;
+
+  }
+
+
+
+  noIgual(control: FormControl): { [s: string]: boolean } {
+
+    let forma: any = this;
+
+    if (control.value !== forma.controls['password1'].value) {
+      return {
+        noiguales: true
+      }
+    }
+
+    return null;
+
   }
 
 }
